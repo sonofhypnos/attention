b4ff03a94ad5ca7a8269b1a0bf4add9346ff90a7
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2022-11-07 23:05:07 +0100

    Add Attention_tunable

--- attention.py
+++ attention.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.nn.parameter import Parameter
+
+# TODO figure out embedding things
 
 
 class Attention(nn.Module):
@@ -9,8 +12,36 @@ class Attention(nn.Module):
 
     def forward(self, Q, K, V):
         # FIXME not sure which is the right way to do the softmax (directly depends on how we arange V?)
+        # FIXME only works for quadratic matrices now
         return torch.softmax(Q @ K.T, 0, torch.float) @ V
         # TODO nice to have would be scaling by sqrt(d_k) (supposedly helpful for gradients)
+
+
+class Attention_tunable(nn.Module):
+    def __init__(self, d_k, d_v, d_model):
+        super(Attention, self)
+        self.w_q = Parameter(torch.empty(d_k, d_model))
+        self.w_k = Parameter(torch.empty(d_k, d_model))
+        self.w_v = Parameter(torch.empty(d_v, d_model))
+
+    def forward(self, Q, K, V):
+        """Allowed myself to look at the torch linear layer to figure out how to do this."""
+        # FIXME not sure which is the right way to do the softmax (directly depends on how we arange V?)
+        # FIXME only works for quadratic matrices now
+        return torch.softmax((Q @ w_q) @ ((K @ w_k).T), 0, torch.float) @ (V @ w_v)
+        # TODO nice to have would be scaling by sqrt(d_k) (supposedly helpful for gradients)
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_k, dv, d_model, h=4):
+        super(MultiHeadAttention, self)
+        # we are making a very small attention head by default. Not sure the dimensions make any sense
+        # TODO do matrix-mult/tensor-dot instead of for-loop:
+        # FIXME figure out if the h_s stuff here will still work
+        self.h_s = []
+
+    def forward(self, Q, K, V):
+        pass
 
 
 def run_single_attention():
@@ -18,7 +49,7 @@ def run_single_attention():
     k = torch.tensor([[1.0, 0.0], [0.0, 1.0]])
     v = torch.tensor([[1.0, 0.0], [0.0, 1.0]])
     attention = Attention()
-    print(attention.forward(q, k, v))
+    return attention.forward(q, k, v)
 
 
 run_single_attention()
--- test_attention.py
+++ test_attention.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import torch
 import attention
 
 
 def test_attention():
-    assert attention.example()
+    result = attention.run_single_attention() - torch.tensor(
+        [[0.7311, 0.2689], [0.2689, 0.7311]]
+    )
+    assert torch.norm(result) < 0.0001
